d8c298a8e35a6126e1d91711d532964391549ee8
Petr Král <kralpet9@fjfi.cvut.cz>
2023-01-18 11:56:56 +0100

    Database now creates & sorts itself if needed

--- src/tetris2/Database.java
+++ src/tetris2/Database.java
@@ -8,28 +8,80 @@ public class Database
 {
     private static final String CONNECTION = "jdbc:mysql://localhost:3306/tetris_leaderboard";
     
-    public static int getTopId()
+    static boolean databaseExists()
+    {
+        boolean canConnect = false;
+        try
+        {
+            Connection connection = DriverManager.getConnection( CONNECTION, "root", "password" );
+            canConnect = true;
+        }
+        catch( SQLException e )
+        {
+            canConnect = false;
+        }
+        return canConnect;
+    }
+    
+    static void createDatabase()
+    {
+        try
+        {
+            Connection connection = DriverManager.getConnection( CONNECTION, "root", "password" );
+            Statement statement = connection.createStatement();
+            statement.executeUpdate( "CREATE DATABASE tetris_leaderboard" );
+        }
+        catch( SQLException e )
+        {
+            e.getStackTrace();
+        }
+    }
+    
+    static void createTable()
+    {
+        try
+        {
+            Connection connection = DriverManager.getConnection( CONNECTION, "root", "password" );
+            Statement statement = connection.createStatement();
+            statement.executeUpdate( "CREATE TABLE leaderboard(id INT PRIMARY KEY, name VARCHAR(20), level INT, score INT);" );
+        }
+        catch( SQLException e )
+        {
+            e.getStackTrace();
+        }
+    }
+    
+    public static void fetchDatabase() // check if the leaderboard already exists, if not, create one
+    {
+        if( !databaseExists() ) 
+        {
+            createDatabase();
+            createTable();
+        }
+    }
+    
+    public static int getTopId() // get the highest id in the current leaderboard
     {
         int id = 0;
         try
         {
             Connection connection = DriverManager.getConnection( CONNECTION, "root", "password" );
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery( "select * from leaderboard" );
+            ResultSet resultSet = statement.executeQuery( "SELECT * FROM leaderboard" );
 
             while ( resultSet.next() )
             {
                 id = resultSet.getInt( "id" );
             }
         }
-        catch( Exception e )
+        catch( SQLException e )
         {
             e.printStackTrace();
         }
         return id;
     }
     
-    public static void insert( String name, int level, int score )
+    public static void insertPlayer( String name, int level, int score )
     {
         int id = getTopId() + 1;
         try
@@ -39,19 +91,19 @@ public class Database
         statement.execute( "INSERT INTO `tetris_leaderboard`.`leaderboard`(`id`,`name`,`level`,`score`)VALUES("
                 + id + "," + "\"" + name + "\"" + "," + level + "," + score + ");" );
         }
-        catch( Exception e )
+        catch( SQLException e )
         {
             e.printStackTrace();
         }
     }
     
-    static void initTable( DefaultTableModel dtm )
+    public static void initTable( DefaultTableModel dtm )
     {
         try
         {
             Connection connection = DriverManager.getConnection( CONNECTION, "root", "password" );
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery( "select * from leaderboard" );
+            ResultSet resultSet = statement.executeQuery( "SELECT * FROM leaderboard" );
 
             while ( resultSet.next() )
             {
@@ -59,20 +111,20 @@ public class Database
                 dtm.addRow( new Object[] { resultSet.getString( "name" ), resultSet.getInt( "level" ), resultSet.getInt( "score" ) } );
             }
         }
-        catch( Exception e )
+        catch( SQLException e )
         {
             e.printStackTrace();
         }
     }
     
-    static void updateTable( DefaultTableModel dtm )
+    public static void updateTable( DefaultTableModel dtm ) // display the most recent entry in the leaderboard
     {
         try
         {
             int id = getTopId();
             Connection connection = DriverManager.getConnection( CONNECTION, "root", "password" );
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery( "select * from leaderboard where id = " + id );
+            ResultSet resultSet = statement.executeQuery( "SELECT * FROM leaderboard WHERE id = " + id );
 
             while ( resultSet.next() )
             {
@@ -80,9 +132,10 @@ public class Database
                 dtm.addRow( new Object[] { resultSet.getString( "name" ), resultSet.getInt( "level" ), resultSet.getInt( "score" ) } );
             }
         }
-        catch( Exception e )
+        catch( SQLException e )
         {
             e.printStackTrace();
         }
     }
+    
 }
